3b783e6e0b2bc432a261a61b15efd144101d9984
Nuwantha Somawansha <e19492@eng.pdn.ac.lk>
2023-12-10 23:53:40 +0530

    routes

--- code/FrontEnd/src/pages/Sign_in.jsx
+++ code/FrontEnd/src/pages/Sign_in.jsx
@@ -11,6 +11,8 @@ import '../css/signin.css';
 import loginimage from '../assets/facerecog.png';
 
 function Signin() {
+
+  
   return (
     <MDBContainer className="my-5 gradient-form" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)'}}>
 
@@ -46,30 +48,6 @@ function Signin() {
         </MDBContainer>
       );
     }
-    {/* <MDBRow> */}
-
-      {/* <MDBCol col='2' className="mb-5"> */}
-
-      // ---
-
-        {/* </MDBCol> */}
-        
-        {/* <MDBCol col='6' className="mb-5">
-          <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
-
-            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-              <h4 class="mb-4">We are more than just a company</h4>
-              <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </div>
-
-          </div>
-
-        </MDBCol> */}
-
-      {/* </MDBRow> */}
 
 
 export default Signin;
