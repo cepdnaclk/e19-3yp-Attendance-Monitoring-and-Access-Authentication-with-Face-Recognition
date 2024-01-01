007359d3b735408ad7e2abcb80fc89b9f276e31d
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-01 09:48:40 +0530

    Update  Motion Detection in Raspberrypi

--- code/raspProject/main.py
+++ code/raspProject/main.py
@@ -1,9 +1,11 @@
+import json
 import subprocess
 import random
 import threading
 import mqtt_communication.config_mqtt as config
 from paho.mqtt import client as mqtt_client
 from control_logic import json_decorder
+from attendance_recognition import motion_detection
 
 broker = config.broker
 port = config.port
@@ -33,16 +35,33 @@ def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         payload = msg.payload.decode()
         print(f"Received `{payload}` from `{msg.topic}` topic")
-        json_decorder.json_decorator(msg)
+
+        json_payload = json.loads(payload)
+
+        if json_payload.get("mode", "").lower() == "configure":
+            print("Configuration Mode Activated")
+            json_decorder.json_decorator(msg)
+
+        elif json_payload.get("mode", "").lower() == "active":
+            print("Active Mode Activated")
 
     client.subscribe(topic)
     client.on_message = on_message
 
 
+def read_input():
+    while True:
+        user_input = input("Enter message to publish to MQTT topic: ")
+        print(user_input)
+
+
 def run():
     client = connect_mqtt()
+    # Start a separate thread for MQTT client
+    mqtt_thread = threading.Thread(target=client.loop_forever)
     subscribe(client)
-    client.loop_forever()
+    mqtt_thread.start()
+    read_input()
 
 
 if __name__ == '__main__':
