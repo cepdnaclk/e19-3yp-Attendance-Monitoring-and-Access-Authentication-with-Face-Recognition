a956ddd1e80ec5d24c4730f0d4cd170c385f466c
Ashan-Wimalasiri <e19452@eng.pdn.ac.lk>
2024-01-07 20:39:28 +0530

    sign up little update

--- code/FrontEnd/src/pages/Sign_up.jsx
+++ code/FrontEnd/src/pages/Sign_up.jsx
@@ -49,7 +49,7 @@ function Signup() {
   }
 
   return (
-    <MDBContainer className="my-5 gradient-form" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)'}}>
+    <MDBContainer className="my-5 gradient-form" style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', maxWidth:'50%'}}>
 
           <div className="d-flex flex-column ms-4 me-4 mt-4 mb-4">
 
@@ -75,10 +75,10 @@ function Signup() {
             onChange={(e) => setGender(e.target.value)}/>
 
 
-            <MDBInput wrapperClass='mb-3' label='Age' id='form4' type='number'
+            <MDBInput wrapperClass='mb-3' label='Age' id='form4' type='text'
             value={age}
             onChange={(e) => setAge(e.target.value)}/>
-            <MDBInput wrapperClass='mb-3' label='Contact number ' id='form5' type='number'
+            <MDBInput wrapperClass='mb-3' label='Contact number ' id='form5' type='text'
             value={number}
             onChange={(e) => setNumber(e.target.value)}/>
             <MDBInput wrapperClass='mb-3' label='Email address' id='form6' type='email'
