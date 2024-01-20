3dc7fe041c8ed9023c43e47ae0610d0ee6dd0fde
L.M.A.H. Premawansha <78247019+aselahemantha@users.noreply.github.com>
2024-01-20 21:16:06 +0530

    Delete code/djangoBackend-3YP-master directory

--- code/djangoBackend-3YP-master/attendanceManagement/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Attendance_Details, Face_Data
-
-# Register your models here.
-
-admin.site.register(Employee)
-admin.site.register(Attendance_Details)
-admin.site.register(Face_Data)
